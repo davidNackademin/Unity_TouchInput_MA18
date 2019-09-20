0b84187d2e7f3c1aa713a70b4e9fa5ccae41df8f
David Svensson <davidsvson@gmail.com>
2019-09-20 09:48:18 +0200

    Change color on touch

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.red;
+    }
+
+
+    private void Update()
+    {
+        foreach( Touch touch in Input.touches)
+        {
+            Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == gameObject )
+            {
+                spriteRenderer.color = Color.yellow;
+                return;
+            }
+        }
+
+        spriteRenderer.color = Color.red;
+
+    }
+
+
+
+
+
+
+}
